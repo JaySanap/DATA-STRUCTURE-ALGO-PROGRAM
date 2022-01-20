b3450a4f0c8a635b49f72f713f6e97301f03bf62
Jay Sanap <90241943+JaySanap@users.noreply.github.com>
2022-01-20 10:47:23 +0530

    Add files via upload

---
+++ LeaderElement.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class LeaderElement {
+    public static void main(String[] args) {
+        int arr[]={2,5,7,9,4,3,1};
+        for (int i=0;i< arr.length;i++)
+        {
+            boolean flag=false;
+            for (int j=i+1;j< arr.length;j++)
+            {
+                if (arr[i]<arr[j]){
+                    flag=true;
+                    break;
+                }
+            }
+
+            if (flag==false){
+                System.out.println(" flag value="+arr[i]);
+            }
+        }
+    }
+}
